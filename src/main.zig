18abb696ef55d12f6fd6b4a85846a0bcf1a84048
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-08-14 22:50:24 +0100

    saving and retreving the current time from disk

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
+const fs = std.fs;
 
 const DAY_SECONDS = 86400;
 const HOUR_SECONDS = 3600;
 const MINUTE_SECONDS = 60;
+const SAVE_FILE = ".30-days";
 
 // Writes seconds into buf in the format:
 //   00 Days, 00 Hrs, 00 Mins, 00 Secs
@@ -26,8 +28,48 @@ fn fmtSeconds(seconds: u64, buf: []u8) !usize {
     return act.len;
 }
 
+fn createNewSave() !void {
+    const now: u64 = @intCast(std.time.timestamp());
+
+    const target = now + 30 * DAY_SECONDS;
+    var buff: [20]u8 = undefined;
+    const slice = try std.fmt.bufPrint(&buff, "{d}", .{target});
+
+    const cwd: std.fs.Dir = std.fs.cwd();
+    const file = try cwd.createFile(SAVE_FILE, .{ .read = true });
+    defer file.close();
+
+    try file.writeAll(slice);
+}
+
+// Gets the current target time from disk.
+// If it cannot find a current timer, a new one of 30days is created.
+// If the current time has expired, a new one of 30days is created.
+fn getTargetTime() !u64 {
+    const cwd: fs.Dir = fs.cwd();
+
+    var target: fs.File = undefined;
+    if (cwd.openFile(SAVE_FILE, .{})) |f| {
+        target = f;
+    } else |err| switch (err) {
+        fs.File.OpenError.FileNotFound => {
+            try createNewSave();
+            target = try cwd.openFile(SAVE_FILE, .{});
+        },
+        else => return err,
+    }
+    defer target.close();
+
+    var buf: [20]u8 = undefined;
+    const amount = try target.readAll(&buf);
+
+    const timestamp = try std.fmt.parseInt(u64, buf[0..amount], 10);
+
+    return timestamp;
+}
+
 pub fn main() !void {
-    const target_time: u64 = @intCast(std.time.timestamp() + 30);
+    const target_time: u64 = try getTargetTime();
 
     while (true) : (std.time.sleep(1e+9)) {
         const current_time: u64 = @intCast(std.time.timestamp());
@@ -35,7 +77,7 @@ pub fn main() !void {
         var buf: [64]u8 = undefined;
         const amount = try fmtSeconds(delta, &buf);
         const formatted = buf[0..amount];
-        std.log.debug("{s}", .{formatted});
+        std.debug.print("{s}\n", .{formatted});
         if (delta <= 0) break;
     }
 }
