46d4f30778b30ad7702907faa8e4ab84cabd44a8
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-08-14 16:33:00 +0100

    adding countdown

--- src/main.zig
+++ src/main.zig
@@ -7,10 +7,10 @@ const MINUTE_SECONDS = 60;
 // Writes seconds into buf in the format:
 //   00 Days, 00 Hrs, 00 Mins, 00 Secs
 fn fmtSeconds(seconds: u64, buf: []u8) !usize {
-    const days_part = seconds / DAY_SECONDS;
-    const hours_part = (seconds % DAY_SECONDS) / HOUR_SECONDS;
-    const minutes_part = ((seconds % DAY_SECONDS) % HOUR_SECONDS) / MINUTE_SECONDS;
-    const seconds_part = ((seconds % DAY_SECONDS) % HOUR_SECONDS) % MINUTE_SECONDS;
+    const days_part = @divFloor(seconds, DAY_SECONDS);
+    const hours_part = @divFloor(@mod(seconds, DAY_SECONDS), HOUR_SECONDS);
+    const minutes_part = @divFloor(@mod(@mod(seconds, DAY_SECONDS), HOUR_SECONDS), MINUTE_SECONDS);
+    const seconds_part = @mod(@mod(@mod(seconds, DAY_SECONDS), HOUR_SECONDS), MINUTE_SECONDS);
 
     const act = try std.fmt.bufPrint(
         buf,
@@ -26,7 +26,21 @@ fn fmtSeconds(seconds: u64, buf: []u8) !usize {
     return act.len;
 }
 
-pub fn main() !void {}
+pub fn main() !void {
+    const target_time: u64 = @intCast(std.time.timestamp() + 30);
+
+    while (true) : (std.time.sleep(1e+9)) {
+        const current_time: u64 = @intCast(std.time.timestamp());
+        const delta = target_time - current_time;
+        var buf: [64]u8 = undefined;
+        const amount = try fmtSeconds(delta, &buf);
+        const formatted = buf[0..amount];
+        std.log.debug("{s}", .{formatted});
+        if (delta <= 0) break;
+    }
+}
+
+// ================ TESTS ===================
 
 test "Fmt Seconds" {
     const expected = "03 Days, 04 Hrs, 23 Mins, 45 Secs";
